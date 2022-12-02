d19cad88e379f5b473e98d9e5cf8e446d949105c
Tigioz <z.fouts@icloud.com>
2022-12-02 15:04:09 -0600

    part 3 complete

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,9 +1,11 @@
 package org.launchcode.techjobs.persistent.models;
 
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -12,6 +14,11 @@ public class Employer extends AbstractEntity {
     @NotBlank
     @Size(min = 1, max = 255)
     private String location;
+
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer() {}
 
