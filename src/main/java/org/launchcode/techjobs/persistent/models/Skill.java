dfbb3cab8478a18459e100ef7947bb36697b1332
Tigioz <z.fouts@icloud.com>
2022-12-02 10:49:53 -0600

    part 2 models: code working

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+
+    @NotBlank
+    @Size(min = 1, max = 255)
+    private String skill;
+
+    public Skill() {}
+
+    public Skill(String skill) {
+        super();
+        this.skill = skill;
+    }
+
+    public String getSkill() {return skill;}
+
+    public void setSkill(String skill) {this.skill = skill;}
 }
